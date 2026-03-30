6c9c25a4df38b2c0560f7d166be9aae553bd326c
Phillip Thapa <1thapat@gmail.com>
2026-03-30 15:50:34 -0400

    set up express server

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db";
+import userRoutes from "./routes/userRoutes";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.send("Server is running");
+});
+
+app.use("/api/users", userRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
